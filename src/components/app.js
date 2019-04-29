97ef862e2f22ade092386ce3b8023b35b2648133
kimbeomgyu <qjarb1994@gmail.com>
2019-04-29 20:18:24 +0900

    react-route 연결

--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return (
-      <div>React simple starter</div>
-    );
+    return <div>React simple starter</div>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import reducers from './reducers';
+import App from './components/app';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <Route exact path="/" component={App} />
+    </Router>
   </Provider>,
   document.querySelector('.container')
 );
