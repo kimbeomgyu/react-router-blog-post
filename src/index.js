0e83090088336f52e6e994a63e12b2b807026c17
kimbeomgyu <qjarb1994@gmail.com>
2019-04-29 20:48:14 +0900

    인덱스 액션

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import reducers from './reducers';
 import App from './components/app';
+import promise from 'redux-promise';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
